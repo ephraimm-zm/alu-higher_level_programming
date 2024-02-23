f65f4d3787824e6ac0406469a77d466df43bb4fd
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-23 08:20:55 -0800

    Attempt to solve task 102

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 98  # LOAD_CONST 1 (98)
+    result += a  # LOAD_FAST a
+    result **= b # BINARY_POWER
+    result += a  # BINARY_ADD
+    return result
