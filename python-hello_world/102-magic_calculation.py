df823d5d50ff27e045d93be58a536b04c871f0d1
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-23 08:22:25 -0800

    Attempt to refactor task 102

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98  # LOAD_CONST 1 (98)
-    result += a  # LOAD_FAST a
-    result **= b # BINARY_POWER
-    result += a  # BINARY_ADD
-    return result
+    return(98 + a ** b)
