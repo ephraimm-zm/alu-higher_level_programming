38dbc858705620a9b07128f46b3555eadf46da09
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-24 14:46:37 -0800

    Added updated solution for task 5 print comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print("0{i}".format(i=i))
+        print("0{i}".format(i=i), end=', ')
     else:
-        print("{i}".format(i=i))
+        print("{i}".format(i=i), end=', ' if i != 99 else '\n')
