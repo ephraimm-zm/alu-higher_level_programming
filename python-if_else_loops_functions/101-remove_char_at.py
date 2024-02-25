29f818068928b2431c2860231cab59f510043e59
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-25 02:24:27 -0800

    Added solution for task 101 remove char using C# stringbuilder type logic

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def remove_char_at(str, n):
+    result = ""
+    count = 0
+    for char in string:
+        if count != n:
+            result += char
+        count += 1
+    return result
