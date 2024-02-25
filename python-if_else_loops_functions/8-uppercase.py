94a9f7434e027927bbb42b28dc7fbf034a58a08a
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-24 23:49:46 -0800

    Updated soltion for 8-uppercase to use .format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     result = ""
     for char in str:
         if 97 <= ord(char) <= 122:
-            result += f"{chr(ord(char) - 32)}"
+            result += "{}".format(chr(ord(char) - 32))
         else:
-            result += f"{char}"
+            result += "{}".format(char)
         print(result)
