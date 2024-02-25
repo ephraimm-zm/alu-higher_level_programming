468d3be516a5231ba356bf01c7d098a0861d64f7
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-25 02:13:55 -0800

    Used stringbuilder method and used .format

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
+    result = ""
     if (122 - i) % 2 == 0:
-        print(chr(i), end='')
+        result += "{}".format(chr(i))
     else:
-        print(chr(i - 32), end='')
+        result += "{}".format(chr(i - 32))
+    print(result)
