23898caa03bb258a1a1f8e6e5432b4ddd382cef6
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-26 02:04:13 -0800

    Added __name__ __main__ thingy

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
+    arg_count = len(argv)
 
-arg_count = len(argv)
-
-if arg_count == 2:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
-elif arg_count > 2:
-    print("{} arguments:".format(arg_count))
-    for i, arg in enumerate(argv[2:], start=1):
-        print("{}: {}".format(i, arg))
-    else:
-        print("0 arguments")
+    if arg_count == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    elif arg_count > 2:
+        print("{} arguments:".format(arg_count))
+        for i, arg in enumerate(argv[2:], start=1):
+            print("{}: {}".format(i, arg))
+        else:
+            print("0 arguments")
