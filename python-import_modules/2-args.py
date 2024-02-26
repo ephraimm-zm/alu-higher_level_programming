e20e160835939bd30b8e40e46da2cc4fd72faa09
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-26 02:06:04 -0800

    Attempt to fix wrong response for 2nd test case

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     from sys import argv
     arg_count = len(argv)
@@ -7,8 +8,9 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(argv[1]))
     elif arg_count > 2:
-        print("{} arguments:".format(arg_count))
-        for i, arg in enumerate(argv[2:], start=1):
+        print("{} arguments:".format(arg_count - 1))  # Subtract 1 to exclude the script name
+        for i, arg in enumerate(argv[1:], start=1):  # Start from index 1 to exclude the script name
             print("{}: {}".format(i, arg))
-        else:
-            print("0 arguments")
+    else:
+        print("0 arguments")
+
