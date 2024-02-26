60f2014b802c36d71142c5b9516f6a3786828964
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-26 07:47:05 -0800

    Added exit 1 when theres something wrong

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,6 +6,7 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     elif len(argv) == 4:
         a = int(argv[1])
         b = int(argv[3])
