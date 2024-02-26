a4076903c57f2582af3312acea4158a80853aedf
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-26 02:07:33 -0800

    Added full stop when 0 arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(argv[1]))
     elif arg_count > 2:
-        print("{} arguments:".format(arg_count - 1))  # Subtract 1 to exclude the script name
-        for i, arg in enumerate(argv[1:], start=1):  # Start from index 1 to exclude the script name
+        print("{} arguments:".format(arg_count - 1))
+        for i, arg in enumerate(argv[1:], start=1):
             print("{}: {}".format(i, arg))
     else:
-        print("0 arguments")
-
+        print("0 arguments.")
