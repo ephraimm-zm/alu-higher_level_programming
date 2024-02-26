7bcee58cce1e7e725e401c2cef0635835ae11d27
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-26 02:01:37 -0800

    Added first attempt for task 2 args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
+
+arg_count = len(argv)
+
+if arg_count == 2:
+    print("1 argument:")
+    print("1: {}".format(argv[1]))
+elif arg_count > 2:
+    print("{} arguments:".format(arg_count))
+    for i, arg in enumerate(argv[2:], start=1):
+        print("{}: {}".format(i, arg))
+    else:
+        print("0 arguments")
