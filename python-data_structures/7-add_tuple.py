eda151c6cb32ac8401343c2097653aa78e77057c
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-27 07:19:45 -0800

    Fixed solution to handle cases when tuple contains less than 2 elements

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    first_a, second_a = tuple_a[:2] + (0, 0)
-    first_b, second_b = tuple_b[:2] + (0, 0)
-
-    first = first_a + first_b
-    second = second_a + second_b
-
-    return (first, second)
+    tuple_c = ()
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    
+    tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return tuple_c
