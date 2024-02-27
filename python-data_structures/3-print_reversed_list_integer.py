ba8757d8b237a46c391f46d315445adf7ace95bf
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-27 05:50:42 -0800

    Attempt to handle when list is none

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return None
+
     for number in reversed(my_list):
         print("{:d}".format(number))
