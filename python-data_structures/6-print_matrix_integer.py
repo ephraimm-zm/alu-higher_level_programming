821d29b0bcc0544ce44d027f08d9236efd542fa6
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-27 06:27:54 -0800

    Changed variable name to make line shorter and pass PEP8 validation

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" " if element != row[-1] else '')
+        for item in row:
+            print("{:d}".format(item), end=" " if item != row[-1] else '')
         print()
