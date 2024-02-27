2869c4e371d4f780be4e6c5f8f532a782767a331
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-27 08:03:05 -0800

    Added soltion for task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def delete_element_at_index(my_list, index):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    new_list = my_list[:idx] + my_list[idx+1:]
+    return new_list
