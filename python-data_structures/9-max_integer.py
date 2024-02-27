447d9ede6864db107d746b45f9095cf92eaf18c0
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-27 07:54:11 -0800

    Added solution for finding max value

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_value = None
+
+    for number in my_list:
+        if max_value is None or number > max_value:
+            max_value = number
+
+    return max_value
