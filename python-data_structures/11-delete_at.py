ed5cf73a0cce6f0257a61c9c2c0167cdcb420864
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-27 08:09:43 -0800

    Changed solution to use del

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@ def delete_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    my_list = my_list[:idx] + my_list[idx+1:]
+    del my_list[idx]
     return my_list
