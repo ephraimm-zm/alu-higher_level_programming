db0cbe3d695a9e0fda74bf3866e154854605fc5c
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-27 07:07:52 -0800

    Added solution for task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    first = tuple_a[0] + tuple_b[0]
+    second = tuple_a[1] + tuple_b[1]
+    tuple_c = (first, second)
+    return tuple_c
