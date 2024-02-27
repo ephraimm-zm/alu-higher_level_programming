ce1c183d4c9e626acab418976fb5481a00dd2490
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-27 08:07:35 -0800

    Changed solution to edit same list instead of makign new one

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@ def delete_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_list = my_list[:idx] + my_list[idx+1:]
-    return new_list
+    my_list = my_list[:idx] + my_list[idx+1:]
+    return my_list
