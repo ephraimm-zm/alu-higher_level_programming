05eb3c317ed73e139bb62f1059dc5e9135088526
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-28 10:11:28 -0800

    Updated solution to handle case when dict is not valid

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,9 @@ def best_score(a_dictionary):
     highest = 0
     highest_key = None
 
+    if not a_dictionary:
+        return None
+
     for key, value in a_dictionary.items():
         if value > highest:
             highest = value
