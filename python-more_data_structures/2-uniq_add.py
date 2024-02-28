55a2116dd28ca93b35569f8e9846c38dd031b467
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-28 08:12:58 -0800

    Added solution for task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique = {}
+    total = 0
+
+    for number in my_list:
+        if number not in unique:
+            unique[number] = True
+            total += number
+    
+    return total
