59146ab10d315cb0fad9ef824107456e2bf786b0
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-28 08:30:43 -0800

    Added solution for task 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    common = set()
+
+    for element in set_1:
+        if element in set_2:
+            common.add(element)
+    result = (set_1.union(set_2)).difference(common)
+
+    return result
