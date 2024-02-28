24b72d33800cd3f3fcdb1c27375af37ef13ee119
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-28 08:03:25 -0800

    Added initial solution for task 1 search replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    result = []
+    for char in my_list:
+        if char == search:
+            result.append(replace)
+        else:
+            result.append(char)
+    return result
