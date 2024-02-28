9503cad87275d8c57e1ee6dd10f142c5312ca13c
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-28 10:09:00 -0800

    Added initial soltion for task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    highest = 0
+    highest_key = None
+
+    for key, value in a_dictionary.items():
+        if value > highest:
+            highest = value
+            highest_key = key
+
+    return highest_key
