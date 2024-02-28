c11e45566769159997226359ba577278ee362845
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-28 09:51:45 -0800

    Fixed syntax error causing wrong output

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value)i:
+def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
     else:
