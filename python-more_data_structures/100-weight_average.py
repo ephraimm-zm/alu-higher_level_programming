d4cfaced646f79b5f03e8d95bd32419241e0e319
ephraimm-zm <e.mulilo1@alustudent.com>
2024-02-29 05:34:51 -0800

    Added initial solution for task 100

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    weight_total = 0
+    scores_total = 0
+
+    for score, weight in my_list:
+        scores_total += score * weight
+        weight_total = weight
+
+    return scores_total / weight_total
