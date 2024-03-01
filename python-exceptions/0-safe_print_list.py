bfba1e14a26f307c48138499222e5a4710e96ea8
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 10:20:22 -0800

    Fixed indentation issue

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,5 +6,5 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[item], end="")
             items_printed += 1
         except Exception:
-    print()
+            print()
     return items_printed
