976faeba62bda04756cbf2e772fb04a30377897d
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 10:19:10 -0800

    Added the try except inside the loop

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     items_printed = 0
-    try:
-        for item in range(x):
+    for item in range(x):
+        try:
             print(my_list[item], end="")
             items_printed += 1
-    except Exception:
-        print()
+        except Exception:
+    print()
     return items_printed
