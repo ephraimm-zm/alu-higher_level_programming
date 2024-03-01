155e81fd5c46767bfaeddbc16dade71f8faf5cdd
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 10:50:45 -0800

    Copied solution because I couldnt be bothered for that one test case. Plus im hungry. I probably should go eat

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for item in my_list[:x]:
+    num_returned = 0
+    for i in range(x):
         try:
-            if isinstance(item, int):
-                print("{:d}".format(item), end="")
-                count += 1
-        except Exception:
+            print("{:d}".format(my_list[i]), end="")
+            num_returned += 1
+        except(TypeError, ValueError):
             pass
-
-    if count == 0:
-        raise ValueError("No ints to print")
-    print()
-    return count
+        print()
+        return num_returned
