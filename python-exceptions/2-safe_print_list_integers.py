70a8b40b2823c8c8cb62d328bda3beaea812fd14
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 12:37:27 -0800

    Attempt to edit range of loop

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for item in my_list[:x]:
+    for i in range(x):
         try:
             print("{:d}".format(item), end="")
             count += 1
