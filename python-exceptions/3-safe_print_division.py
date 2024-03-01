ba5e4ad802de7252843e0f3fea665aad34930281
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 12:52:53 -0800

    Added initial solution for task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    excpet Exception:
+        return None
+    finally:
+        print("Inside result:{}".format(result))
