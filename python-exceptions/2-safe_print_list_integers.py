d621bb8417ef91034de09489593cd26fd31e4696
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 10:38:24 -0800

    Added initial solution for task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for item in my_list[:x]:
+        try:
+            if isinstance(item, int):
+                print("{:d}".format(item), end="")
+                count += 1
+        except Exception:
+            pass
+    return count
