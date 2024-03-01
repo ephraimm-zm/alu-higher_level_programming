84de79cc78c17f9e438cf2196145d7bef3b6e740
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 13:44:45 -0800

    Added finally block

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,4 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
-    return result
+    finally:
+        return result
