58d3699d2876f0131e0469e9aa920a6a3d1ecf5c
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 10:40:45 -0800

    Changed except params

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(item, int):
                 print("{:d}".format(item), end="")
                 count += 1
-        except Exception:
+        except(TypeError, ValueError):
             pass
     print()
     return count
