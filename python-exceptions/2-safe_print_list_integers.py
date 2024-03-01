799b3bf1e77bb90553493b68013094c78e975a52
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 10:45:08 -0800

    I dont understand why one test case is failing

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         except Exception:
             pass
-    if count == 0:
-        raise ValueError
     print()
     return count
