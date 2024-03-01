b66ab56b704a260e885a63b55e3877f9fe41c4a9
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-01 10:21:57 -0800

    f this ish

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,4 +6,5 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[item], end="")
             items_printed += 1
         except Exception:
+            break
     return items_printed
