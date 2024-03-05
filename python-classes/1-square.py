097caf9f43684fd73811dae565b85491ddbddfd8
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-05 09:57:57 -0800

    Added solution for task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""
+This script defines a Square class representing a square shape.
+"""
+
+class Square:
+    
+    """
+    Represents a square object.
+
+    Attributes:
+    __size (int): The size of the square/
+
+    Methods:
+    __init__: Initializes a Square object with a given size.
+    """
+
+def __init__(self, size):
+    """
+    Initializes a Square object with a given size
+
+    Parameters:
+    size (int): The size of the square/
+    """
+
+    self.__size = size
