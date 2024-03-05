725c0b8dcc86e0e94d193bac6f2e47d3a3d93294
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-05 10:22:32 -0800

    Added initial soltuoin for task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""
+This script defines a Square class representing a square shape.
+"""
+
+
+class Square:
+    """"
+    Represents a square object.
+
+    Attributes:
+    __size (int): The size of the square.
+
+    Methods:
+    __init__: Initializes a Square object with a given size.
+    area: Calculates the area of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square object with a given size.
+
+        Parameters:
+        size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+        int: The area of the square.
+        """
+        return self.__size ** 2
