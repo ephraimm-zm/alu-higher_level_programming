09a66f44a49444541e078c6144f821f8aa5954ff
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-06 09:58:53 -0800

    Attempt to raise data setter error everytime

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -45,9 +45,7 @@ class Node:
         Raises:
         TypeError: If the value is not an int
         """
-        if not isinstance(value, int):
-            raise TypeError("data must be an integer")
-        self.__data = value
+        raise TypeError("data must be an integer")
 
     @property
     def next_node(self):
