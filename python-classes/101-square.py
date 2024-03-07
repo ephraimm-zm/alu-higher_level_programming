c13ff2e0b5e3d17b89ac86ba9d173f7249106a3b
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-07 07:02:09 -0800

    Attempt to remove last line out of loop

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -97,7 +97,7 @@ class Square:
             print()
         for _ in range(self.size):
             print(" " * self.position[0] + "#" * self.size)
-            print("\r", end="", flush=True)
+        print("\r", end="", flush=True)
 
     def __str__(self):
         """
