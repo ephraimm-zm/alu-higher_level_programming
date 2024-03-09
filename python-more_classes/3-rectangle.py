1db917dff9a4b7b25055d8d0699be4e848d54c0a
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-09 02:10:05 -0800

    Fixed logical syntax error

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__value = value
+        self.__width = value
 
     @property
     def height(self):
