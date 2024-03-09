72fda266ee5b032f7c74fbd03105023cdbc06189
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-09 08:08:10 -0800

    Fixed indentation issues

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -39,54 +39,54 @@ class Rectangle:
         if value < 0:
             self.__width = value
 
-        @property
-        def height(self):
-            """
-            int: The height of the rectangle.
-            """
-            return self.__height
+    @property
+    def height(self):
+        """
+        int: The height of the rectangle.
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """
-            Set the height of the rectangle.
+    @height.setter
+    def height(self, value):
+        """
+        Set the height of the rectangle.
 
-            Args:
-            value (int): The height value to set.
-            """
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+        Args:
+        value (int): The height value to set.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-        def area(self):
-            """
-            Calculate the area of the rectangle.
-            """
-            return self.__width * self__height
+    def area(self):
+        """
+        Calculate the area of the rectangle.
+        """
+        return self.__width * self__height
 
-        def perimeter(self):
-            """
-            Calculate the perimeter of the rectangle.
-            """
-            if self.__width == 0 or self.__height == 0:
-                return 0
-            return 2 * (self.__width + self.__height)
+    def perimeter(self):
+        """
+        Calculate the perimeter of the rectangle.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
 
-        def __str__(self):
-            """
-            Return a string representation of the rectangle.
-            """
-            if self.__width == 0 or self.__height == 0:
-                return ""
-            result = ""
-            for _ in range(self.__height):
-                result += "#" * self.__width + "\n"
-            return result[:-1]
+    def __str__(self):
+        """
+        Return a string representation of the rectangle.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        result = ""
+        for _ in range(self.__height):
+            result += "#" * self.__width + "\n"
+        return result[:-1]
 
-        def __repr__(self):
-            """
-            Return a string representation for recreation.
-            """
-            return f"Rectangle({self.__width}, {self.__height})"
+    def __repr__(self):
+        """
+        Return a string representation for recreation.
+        """
+        return f"Rectangle({self.__width}, {self.__height})"
