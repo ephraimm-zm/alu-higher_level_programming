d94088b4d2414bb87bc70b633dc933771c12d7e9
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-09 08:17:43 -0800

    Added value error

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -37,7 +37,8 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            self.__width = value
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
