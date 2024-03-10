771d970d07882f0ec7eec031aa8bf150ea403da4
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-09 23:35:12 -0800

    Fixed name from number_of_rectangles to number_of_instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     """
     A class representing a rectangle.
     """
-    number_of_rectangles = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
@@ -20,7 +20,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_rectangles += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -100,4 +100,4 @@ class Rectangle:
         Prints message when an instance of rectangle is deleted
         """
         print("Bye rectangle...")
-        Rectangle.number_of_rectangles -= 1
+        Rectangle.number_of_instances -= 1
