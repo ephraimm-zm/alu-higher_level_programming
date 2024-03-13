4ff9b94b7c2483387dc064f0a5717b503a179c1d
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-13 10:46:32 -0700

    Documented module and class

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a LockedClass.
+"""
 class LockedClass:
+    """
+    Class to only permit instance attrributes named first_name
+    
+    Attributes:
+    __slots__: A list containing the allowed attribute names.
+    """
     __slots__ = ['first_name']
