7405fa01f6cc274c87bbbf0d27090f1e9969f525
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-13 06:43:13 -0700

    Last correction was one line too long

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    return "Holberton" + ", " * magic_string.count
-magic_string.count = 0
-magic_string.count += 1
+def magic_string(count=[-1]):
+    count[0] += 1
+    return "Holberton" + ", Holberton" * count[0]
