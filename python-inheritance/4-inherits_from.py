41931fc35a817582bb67aafe86a1bf91383ebb8c
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-16 04:49:56 -0700

    Documented module

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""
+if obj is instance of a class that inherited
+from the specified class.
+"""
+
+
 def inherits_from(obj, a_class):
     """
-    If obj is instance of a class that inherited
-    from the specified class.
-
     Args:
         obj: The object to check
         a_class: The class to check against
