00883519dac25352fcc4991940b8fb3ecc9b1e7d
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-16 04:55:53 -0700

    Fix syntax error

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -15,4 +15,4 @@ def inherits_from(obj, a_class):
         True if the object is an instance of a class
         that inherited, otherwise False
     """
-    return issubclass(obj.__class__, a_class and obj.__class__ != a_class)
+    return issubclass(obj.__class__, a_class) and obj.__class__ != a_class
