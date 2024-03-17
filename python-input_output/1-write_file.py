fcf73b9af21b0ce84c1bf3a9d1d2eff258345716
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-17 06:53:05 -0700

    Added initial solution for task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Module to write or override text to a specified file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Args:
+        filename (str): The path to the file to be written
+        text (str): The text to be writtted to the file.
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
