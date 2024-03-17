52a540487a56f6132394ba626ede7f5c757bd155
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-16 23:02:36 -0700

    Added initial solution for task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+Module for task 10.square.pu
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square
+
+    Attributes:
+        __size (int) : The length of one side of the square
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square object with a given size
+
+        Args:
+            size (int): The given size of the square/
+        """
+        size.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+
+        Returns:
+            int: The area
+        """
+        return self.__size ** 2
