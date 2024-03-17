e0ae33ecddcf922ce43d8b76fcb231af6f5e2ffd
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-16 23:54:31 -0700

    Added initial solution for task 100

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Module for task 100
+"""
+
+
+class MyInt(int):
+    """
+    A class that inherits from another but overrides things
+    """
+
+    def __eq__(self, other):
+        """
+        Inverts the behaviour of == operator.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Inverts the behaviour of != operator.
+        """
+        return super().__eq__(other)
