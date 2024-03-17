b609e510cc4b2865dc241540955a6f8700245bff
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-17 08:00:06 -0700

    Added initial solution for task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Converts JSON string to a Python object.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Args:
+        my_str (str): The JSON string
+    Returns:
+        obj: The python object
+    """
+    return json.loads(my_str)
