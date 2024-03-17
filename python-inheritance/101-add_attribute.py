617e21dd12273a05a5b65bc519af462c7026ea04
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-17 00:10:33 -0700

    Added initial solution for task 101

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+Module for task 101
+"""
+
+
+def add_attribute(obj, attr_name, attr_value):
+    """
+    Adds a new attribute to an object if possible
+
+    Args:
+        obj: The object to which the attribute will be added
+        attr_name (str): The name of the new attribute
+        attr_value: The value of the new attribiute.
+
+    Raise:
+        TypeError: If object cannot have a new attribute
+    """
+
+    if not hasattr(obj, '__dict__') and not hasattr(obj, '__slots__'):
+        raise TypeError("can't add new attribute")
+
+    setattr(obj, attr_name, attr_value)
