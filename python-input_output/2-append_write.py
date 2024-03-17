71f81b6eb614a3ad38a1c1554ffdbcc2748cf654
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-17 07:09:09 -0700

    Modified docustrings for PEP8

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,9 +6,11 @@ Appends a string at the end of a text file.
 
 def append_file(filename="", text=""):
     """
+    Appends the specified text to the end of the specified file.
+
     Args:
-        filename (str): The path to the file
-        text (str): The text to be appended
+        filename (str): The path to the file.
+        text (str): The text to be appended.
     """
     with open(filename, 'a', encoding='utf-8') as file:
         file.write(text)
