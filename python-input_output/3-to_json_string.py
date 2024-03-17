69672617218c3c0658a742fc6a58cbe9b5be4dd4
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-17 07:54:47 -0700

    Added initial solution for task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import json
+"""
+Converts a string object to JSON
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Args:
+        string (str): The string object to convert
+
+    Returns:
+        str: JSON representation.
+    """
+    return json.dumps(my_obj)
