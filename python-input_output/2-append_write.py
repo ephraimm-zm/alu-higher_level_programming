0d4c200c36421953f08c6f0c5ea6cd89635f1696
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-17 07:05:47 -0700

    Added solution for task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Appends a string at the end of a text file.
+"""
+
+
+def append_file(filename="", text=""):
+    """
+    Args:
+        filename (str): The path to the file
+        text (str): The text to be appended
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.write(text)
