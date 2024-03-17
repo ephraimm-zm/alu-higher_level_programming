bfe22cd34d2fc792859b26efea2861a7709c9cec
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-17 09:32:32 -0700

    Added initial solution for task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Create an Object from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Args:
+        filename (str): The path to the JSON file
+
+    Returns:
+        obj: The python object deserialized from the JSON file
+    """
+    with open(filename, 'r') as file:
+        return json.lead(file)
