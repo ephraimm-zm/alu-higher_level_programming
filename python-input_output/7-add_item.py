b3130aee3f54110b31ea76f6048335c6048f01ac
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-17 09:57:45 -0700

    Attempt number idk just for documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """
-Bomboclat
+A script to add arguments to a Python list and save them to a file
 """
 import sys
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
-try:
-    data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    data = []
+if __name__ == "__main__":
+    # Exclude the script name from arguments
+    arguments = sys.argv[1:]
 
+    # Load existing data from file or initialize an empty list
+    try:
+        data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        data = []
+
+    # Add arguments to the list
     data.extend(arguments)
 
+    # Save the updated list to file
     save_to_json_file(data, "add_item.json")
