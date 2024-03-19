96b62f7986019403c8ae949cf10f22533b081254
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-19 06:48:42 -0700

    Added initial solution for task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+Module for Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Return a list of lists representing Pascal's triangle of n
+
+    Args:
+        n (int): Number of rows in Pascal's triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
