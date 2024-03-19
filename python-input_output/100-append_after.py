bd467c6613fe8f1e7763bf75dfc13673382467b1
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-19 06:57:31 -0700

    Added initial solution for task 100

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+Module for task 100
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Append a string after a specific string in a file
+
+    Args:
+        filename (str): The name of the file to be modified
+        search_string (str): The string to search for in the file
+        new_string (str): The string to append after the search_string
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
