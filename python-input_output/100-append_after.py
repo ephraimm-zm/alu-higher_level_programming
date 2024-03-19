b3ffbad01a924353a9f6704a6285e76314f8cc67
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-19 06:58:37 -0700

    Removed new line after adding the new string

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -20,4 +20,4 @@ def append_after(filename="", search_string="", new_string=""):
         for line in lines:
             file.write(line)
             if search_string in line:
-                file.write(new_string + '\n')
+                file.write(new_string)
