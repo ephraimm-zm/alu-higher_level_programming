0bdf152b7770ca2019d66377baa47bf073046c25
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-19 06:32:42 -0700

    Added solution from task 10 as base for task 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""
+Module for task 10
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name (str): First name of student
+            last_name (str): Last name of student
+            age (int): Age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns:
+            dict: Dictionary representation of Student instance
+
+        Args:
+            attrs (list): Optional list of attribute names
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
