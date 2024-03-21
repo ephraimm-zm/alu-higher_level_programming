64b61f4c17f7c7098181dc306c8c15c566cad969
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-21 07:29:21 -0700

    Changed solution for task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
 -- Script to print description of table in db
-USE information_schema;
-
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' and TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table
