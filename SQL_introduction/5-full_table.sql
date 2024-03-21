468f6f0d347b99bb3024760b346387f53a167184
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-21 07:19:50 -0700

    Added information schema

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,6 @@
 -- Script to print description of table in db
+USE information_schema;
+
 SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
 FROM COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' and TABLE_NAME = 'first_table';
