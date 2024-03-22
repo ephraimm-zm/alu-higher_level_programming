76c4f69dc636ea2be7290df411688c2a62d98999
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-22 08:32:35 -0700

    Attempt to group values as number

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Lists number of records with the same score
-SELECT score, COUNT(score) AS score_count
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
-ORDER BY score_count DESC;
+ORDER BY number DESC;
