acd3bdb31fa742ae79af64ba98796102d7e16a18
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-22 08:31:18 -0700

    Attempt to fix output as expected

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Lists number of records with the same score
-SELECT COUNT(score) AS score_count
+SELECT score, COUNT(score) AS score_count
 FROM second_table
 GROUP BY score
 ORDER BY score_count DESC;
