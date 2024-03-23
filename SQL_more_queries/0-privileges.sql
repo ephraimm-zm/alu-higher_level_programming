fc7595026f67155aea5ac302621fec23cdcbff52
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-23 07:51:35 -0700

    Attempt to fix solution for task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
 -- Script to list all privelleges of the states users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Grants for user_0d_1@localhost
+SELECT CONCAT('Grants for user_0d_1@localhost\n', SHOW GRANTS FOR 'user_0d_1'@'localhost') AS user_0d_1_grants;
+
+-- Grants for user_0d_2@localhost
+SELECT CONCAT('Grants for user_0d_2@localhost\n', SHOW GRANTS FOR 'user_0d_2'@'localhost') AS user_0d_2_grants;
