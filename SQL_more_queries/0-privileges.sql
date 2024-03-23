4ba9fb29497e5b2dc4813aba5334625fe0d99d4c
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-23 07:53:33 -0700

    Why is it working for one user but not both and why is the output so long it doesn't even show everything and why can't i download the error message log

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,3 @@
 -- Script to list all privelleges of the states users
--- Grants for user_0d_1@localhost
-SELECT CONCAT('Grants for user_0d_1@localhost\n', SHOW GRANTS FOR 'user_0d_1'@'localhost') AS user_0d_1_grants;
-
--- Grants for user_0d_2@localhost
-SELECT CONCAT('Grants for user_0d_2@localhost\n', SHOW GRANTS FOR 'user_0d_2'@'localhost') AS user_0d_2_grants;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
