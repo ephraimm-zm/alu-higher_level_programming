ab3411230a1d1df43d7372efb380dc42c9587c7b
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-23 07:56:15 -0700

    Removed single quotation marks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Script to list all privelleges of the states users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
