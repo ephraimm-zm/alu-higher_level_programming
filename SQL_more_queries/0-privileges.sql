ac7f4b3d6118cfe4a6289c3e13c9bbe33f4996fd
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-23 07:39:01 -0700

    Attempt to fix solution for task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- Script to list all privelleges of the states users
-SELECT *
-FROM information_schema.user_privileges
-WHERE grantee IN ('user_0d_1@localhost', 'user_0d_2@localhost');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
