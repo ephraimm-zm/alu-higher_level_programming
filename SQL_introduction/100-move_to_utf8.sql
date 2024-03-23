1530495696cbb2d4ebeb77ab110a660ace908730
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-23 00:48:49 -0700

    Attempt to remove the second char set to utfm8mb4

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -5,7 +5,6 @@ ALTER DATABASE hbtn_0c_0
 
 -- Convert the table to UTF8
 ALTER TABLE hbtn_0c_0.first_table 
-    CONVERT TO CHARACTER SET utf8mb4 
     COLLATE utf8mb4_unicode_ci;
 
 -- Modify the collation of the name field to utf8mb4_unicode_ci
