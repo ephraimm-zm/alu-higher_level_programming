a82cd109963fe69f82d333c7b14dcd4a28d0be4b
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 08:04:06 -0700

    Added initial solution for task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,1 +1,9 @@
---
+-- Script to create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL UNIQUE,
+	name VARCHAR(256)
+	);
