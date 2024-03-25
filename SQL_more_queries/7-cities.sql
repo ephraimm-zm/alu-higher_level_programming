41e34300af2e5e6bb66c49ef382d61cfc3a3dfde
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 08:14:54 -0700

    Added initial solution for task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,13 @@
---
+-- Create a database if it doesn't existss
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the db
+USE hbtn_0d_usa;
+
+-- Create table if not exists with foreign key
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+	);
