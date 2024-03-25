5007cbe0633af0ea93bea078babdaf9729ebf195
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 15:36:37 -0700

    Added initial solution for task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,1 +1,7 @@
---
+-- Script to list all genres and display number of shows linked
+SELECT genre AS genre, COUNT(tv_shows.title) AS number_of_shows
+FROM genres
+JOIN tv_shows_genres ON genres.id = tv_shows_genres.genre_id
+JOIN tv_shows ON tv_shows_genres.tv_show_id = tv_shows.id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
