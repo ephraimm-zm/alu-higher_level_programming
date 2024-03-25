87bdfb4ce537633223ffee3d68a097107a3c7cca
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 16:20:46 -0700

    Edited solution to select DISTINCT values

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,5 +1,5 @@
 -- Script to list all genres not linked to the show Dexter
-SELECT tv_genres.name
+SELECT DISTINCT tv_genres.name
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id AND tv_shows.title = 'Dexter'
