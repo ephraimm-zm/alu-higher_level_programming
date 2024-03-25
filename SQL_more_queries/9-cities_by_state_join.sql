5c766756166d673395fedf3c55558233ea014207
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 09:39:31 -0700

    Added initial solution for task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,5 @@
---
+-- Script to list cities with their corresponding state names, in asc order
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
