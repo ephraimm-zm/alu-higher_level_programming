6606190b4629db498179bd0e1d4a9e0b8ca45b6e
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 16:07:34 -0700

    Added solution for task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,1 +1,6 @@
---
+-- Script to list TV shows with their genres and NULL if null
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC;
