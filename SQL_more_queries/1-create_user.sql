bb6809c4bce99378683444ae883ae49422a44fb9
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 01:07:16 -0700

    Added inial solution for task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,1 +1,7 @@
---
+-- Creates new user if not exists and give priveleges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+FLUSH PRIVILEGES;
