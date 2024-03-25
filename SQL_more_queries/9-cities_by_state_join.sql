afe9abe3a71b3e8800bc14ad812138f0934963ce
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 09:40:37 -0700

    Changed name of state on output

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Script to list cities with their corresponding state names, in asc order
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name AS name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
