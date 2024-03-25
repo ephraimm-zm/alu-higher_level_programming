09caad99f7d2ed6573ab9d388a537a977e34ce40
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 10:37:03 -0700

    Put the ORDER BY in the main query

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Script to list all shows and genres, displaying null if genre doesn't exist
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, NULL) AS genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, IFNULL(tv_show_genres.genre_id, NULL) ASC;
