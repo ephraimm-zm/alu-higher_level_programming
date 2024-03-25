116b7005c94c8958b922b947aa1577f82915d0a4
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-25 09:32:07 -0700

    Added initial solution for task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,1 +1,9 @@
---
+-- Script to list all cities of California sorted in ASC by cities id
+SELECT id, name
+FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+)
+ORDER BY id ASC;
