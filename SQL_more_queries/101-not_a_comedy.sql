44b008e386cb64fdd6a344b8a31c23c5eeb35bf7
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-26 00:29:38 -0700

    Fix syntax error

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -5,7 +5,6 @@ LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.id NOT IN (
 	SELECT tv_shows.id
-	FROM tv_shows.id
 	FROM tv_shows
 	JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 	JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
