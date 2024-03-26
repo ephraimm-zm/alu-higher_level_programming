af23c8e8ae48c2013c953cedca582e851cfe5750
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-26 00:05:26 -0700

    Updated solution to perfom LEFT JOIN with a subquery to select IDs associated with the show Dexter

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,12 @@
 -- Script to list all genres not linked to the show Dexter
-SELECT tv_genres.id, tv_genres.name
+SELECT DISTINCT tv_genres.id, tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id AND tv_shows.title = 'Dexter'
-WHERE tv_shows.id IS NULL
-ORDER BY tv_genres.name
+LEFT JOIN (
+	SELECT tv_genres.id
+	FROM tv_genres
+	JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = 'Dexter'
+) dexter_genres ON tv_genres.id = dexter_genres.id
+WHERE dexter_genres.id IS NULL
+ORDER BY tv_genres.name;
