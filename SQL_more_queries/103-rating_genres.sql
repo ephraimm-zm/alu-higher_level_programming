31bd8df64fea7e623e085b7994f7769f0cd86b29
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-26 00:50:34 -0700

    Updated solution to sort results in DESC instead of ASC

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -5,4 +5,4 @@ JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
-ORDER BY rating ASC;
+ORDER BY rating DESC;
