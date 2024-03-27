8d2286e575cc544a3c76ccdd217d8d4b430d31f6
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-27 15:44:00 +0000

    Changed text from you got me to you find me

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request with specific data to target URL
-curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "0.0.0.0:5000/catch_me" --output /dev/null -w "You got me!"
+curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "0.0.0.0:5000/catch_me" --output /dev/null -w "You find me!"
