4c5b933054ba5aa90d49e61c2124a08e443d1f1c
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-28 14:38:21 +0000

    Added initial solution for task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Take URL, send req to URL and display value of X-Request-Id
+"""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+if __name__ == "__main__":
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+       print(response.getheader("X-Request-Id")
