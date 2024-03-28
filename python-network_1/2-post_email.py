727c942920e0c42a5125b855c22296c56aba2977
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-28 15:01:42 +0000

    Added initial solution for task 1

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Take URL and email, send POST, display body decoded in utf-8
+"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    req = urllib.request.Request(url, data=email.encode('utf-8'))
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8')
