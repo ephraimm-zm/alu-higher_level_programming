ed07f1774213de57299561ae8911680a34aa2654
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-28 11:12:57 +0000

    Attempt number one million at task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
 """
-This script fetches the status from a specified URL using the urllib package
-and displays information about the response body.
-
-Usage:
-    python3 script_name.py
-
-Dependencies:
-    - urllib (part of the Python standard library)
-
-Example:
-    $ python3 script_name.py
+Fetch url
 """
-
 import urllib.request
 
-def main():
-    # URL to fetch status from
-    url = "https://alu-intranet.hbtn.io/status"
-
-    # Fetch the URL and process the response
-    with urllib.request.urlopen(url) as response:
-        # Read the body of the response
-        body = response.read()
-
-        # Display information about the response body
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode('utf-8'))
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+	url = "https://alu-intranet.hbtn.io/status"
 
 if __name__ == "__main__":
-    main()
+	req = urllib.request.Request(url)
+	with urllib.request.urlopen(req) as response:
+		content = response.read()
+		print("Body response:")
+		print("\t- type:", type(content))
+		print("\t- content:", content)
+		print("\t- utf8 content:", content.decode(utf-8))
