80c05d6de30ad30c8779c5b5094b4f8eed9eb6ab
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-29 15:16:18 +0000

    Changed location of req

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -8,8 +8,8 @@ import urllib.error
 
 
 if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
     try:
-        req = urllib.request.Request(sys.argv[1])
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
