3613bfb2e9f80102af93a7d136caead6b0cdf6a7
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-29 11:56:28 +0000

    Added initial solution for task 3

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Take a URL, send request, display body decoded in utf-8
+"""
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    try:
+        req = urllib.request.Request(sys.argv[1])
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print(f"HTTP Error: {e.code} - {e.reason}")
