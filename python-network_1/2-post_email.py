d64b88d0dfef665768715575e15433c9c85a4214
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-29 11:40:16 +0000

    Updated solution for mmodule documentation

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,17 +2,16 @@
 """
 Take URL and email, send POST, display body decoded in utf-8
 """
-import urllib.request
 import sys
+import urllib.parse
+import urllib.request
+
 
 if __name__ == "__main__":
-"""
-documentaion bloody
-"""
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    req = urllib.request.Request(url, data=email.encode('utf-8'))
-
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
