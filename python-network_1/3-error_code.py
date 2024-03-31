42af392685fa5c704b797da8d94a819470b3c367
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 10:46:25 +0000

    Added req var

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -11,6 +11,7 @@ import urllib.error
 if __name__ == "__main__":
 
     try:
+        req = urllib.request.Request(sys.argv[1])
         with request.urlopen(sys.argv[1]) as resq:
             print(resq.read().decode('utf-8'))
     except error.HTTPError as e:
