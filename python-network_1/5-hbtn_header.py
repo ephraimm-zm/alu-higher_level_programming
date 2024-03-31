daa6e1987bb396d45963e04028d22497b3e1ec0b
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 13:56:34 +0000

    Added rest of code hoping documentation error fixes itself

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -4,3 +4,8 @@ Takes URL, sends request, display value of X-Request-Id
 """
 import requests
 import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers['X-Request-Id'])
