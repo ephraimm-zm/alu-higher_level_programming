22b153d15a12930cb103654f4748942ca00c5979
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 14:12:38 +0000

    Correctly formated email as dictionary

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    response = requests.post(url, email)
+    email = {'email': sys.argv[2]}
+    response = requests.post(url, data=email)
     print(response.text)
