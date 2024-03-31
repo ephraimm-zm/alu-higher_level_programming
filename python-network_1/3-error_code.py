55c784556a296b92240fb9d0f11a6fcda7d153fe
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 10:55:54 +0000

    Changed last print statement

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
-        print(f"Error: {e.code}")
+        print("Error code:", e.code)
