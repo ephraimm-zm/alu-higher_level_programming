34d56d1d3c43165701e1f127c53f2be67257bd01
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 10:50:22 +0000

    Added new print statement for success

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     try:
         req = urllib.request.Request(sys.argv[1])
         with urllib.request.urlopen(req) as response:
-            print(resq.read().decode('utf-8'))
+            print(response.read().decode("utf-8"))
     except error.HTTPError as e:
         print('Error code:', e.code)
