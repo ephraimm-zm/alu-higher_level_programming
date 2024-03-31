5c6d10e301d80701942e02b975571027b4a0b3c2
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 11:58:18 +0000

    Changed url

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ Fetch url and display status
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type:", type(response.text))
     print("\t- content:", response.text)
