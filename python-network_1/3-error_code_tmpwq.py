f0d7897274356bd9c8ebfeaf596d962d347b1a36
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 11:06:53 +0000

    Attempt to reupload first solution without f string

--- python-network_1/3-error_code_tmpwq.py
+++ python-network_1/3-error_code_tmpwq.py
@@ -6,12 +6,11 @@ Usage: ./3-error_code <URL>
 import sys
 import urllib.request
 import urllib.error
-
-
 if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
+
     try:
+        req = urllib.request.Request(sys.argv[1])
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
-        print("Error code", e.code)
+        print("Error code:", e.code)
