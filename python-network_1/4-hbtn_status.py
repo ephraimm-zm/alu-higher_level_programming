05222e0d2e5c8456511966e829e94179a549bc9e
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 11:44:47 +0000

    Removed all code except import

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,10 +4,3 @@ Fetch url and display status
 """
 import requests
 
-if __name__ == "__main__":
-
-    response = requests.get("https://alu-intranet.hbtn.io/status")
-
-    print("Body response:")
-    print("\t - type:", type(response)
-    print("\t - content:", response.content)
