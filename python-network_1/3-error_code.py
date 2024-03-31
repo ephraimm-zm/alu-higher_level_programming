cd49f2461f1a3e2a037c18f279bc40b1f0be10c1
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 10:33:07 +0000

    Attemmpt to remove req

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -8,7 +8,6 @@ Usage: ./3-error_code <URL>
 if __name__ == "__main__":
     import sys
     import urllib.request, error
-    req = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("utf-8"))
