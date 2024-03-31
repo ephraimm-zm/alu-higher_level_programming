e53ab1194af6f21e70bdedc59ad7ecd377aa641b
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 11:39:57 +0000

    Added intiial solution for task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Fetch url and display status
+"""
+import requests
+
+response = requests.get("https://alu-intranet.hbtn.io/status")
+
+print("Body response:")
+print("\t - type:", type(response)
+print("\t - content:", response.content)
