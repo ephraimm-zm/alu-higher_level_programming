adb392588d6e7bf50baf4fe6b41dcd951ea07734
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 11:09:49 +0000

    Changed name and reverted to optimised solution

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -6,11 +6,10 @@ Usage: ./3-error_code <URL>
 import sys
 import urllib.request
 import urllib.error
-
-
 if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
+
     try:
+        req = urllib.request.Request(sys.argv[1])
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
