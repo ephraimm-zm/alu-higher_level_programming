5dda11e9314fbb510ef34950f95d37c3df291f27
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 21:02:04 +0000

    Just to test if f string will cause not documented error

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Take GitHub user and repo name, list 10 commits in desc order
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    repo = sys.argv[1]
+    repo = sys.argv[2]
+    url = f"https://api.github.com/repos/{user}/{repo}/commits"
+
+
