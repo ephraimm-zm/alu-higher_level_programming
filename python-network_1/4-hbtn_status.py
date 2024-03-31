e560ec40d993f702c2698ac2c61d2b8a52e378dd
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 11:56:40 +0000

    Removed leading and trailing spaces

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ import requests
 if __name__ == "__main__":
     response = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t - type: ", type(response.text))
-    print("\t - content: ", response.text)
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
