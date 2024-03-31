3e1f3063d2d5c5f59d6ce5510d86fe175472b3d5
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 20:27:05 +0000

    Added initial solution for task 8

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Takes letter, sends POST with letter as param
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data={'q': q})
+
+    try:
+        data = response.json()
+        if not data:
+            print("No result")
+        else:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
