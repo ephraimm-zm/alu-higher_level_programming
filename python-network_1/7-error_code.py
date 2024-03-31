4a846e0dd23e48f7bcf329297bf17acfe6b8c9e8
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 14:38:05 +0000

    Added initial solution for task 7

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Take URL, send request, display body based on logic
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
