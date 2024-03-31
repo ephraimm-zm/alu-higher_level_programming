d802f72a8ca6083dee57bccd9c8c0e991aea5c28
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 20:44:43 +0000

    Added initial solution for task 10

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Take Github credentials, use GitHub API, display id
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, password))
+    user_data = response.json()
+    try:
+        print(user_data['id'])
+    except:
+        pass
