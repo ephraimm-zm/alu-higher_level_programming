66871df3f4af6109d37b59626eae58d9abd044d2
ephraimm-zm <e.mulilo1@alustudent.com>
2024-03-31 14:09:19 +0000

    Added initial solution for task 6

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Take URL and email, send POST, display body of response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    response = requests.post(url, email)
+    print(reponse.text)
