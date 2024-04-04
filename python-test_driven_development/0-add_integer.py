ba50c64101a0b691e33258995711e88aa2447eca
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-04 11:36:03 -0700

    Added initial function and tests for task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+This module provides funtion for adding two integers
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers
+
+    Returns:
+    int: Su of two ints
+
+    Raises:
+    TypeError: If either a or b cannot be cast to an int
+    """
+    try:
+        a = int(a)
+    except:
+        raise TypeError('a must be an integer')
+
+    try:
+        b = int(b)
+    except:
+        raise TypeError('b must be an integer')
+
+    return a + b
