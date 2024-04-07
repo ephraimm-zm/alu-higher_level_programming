6acc0a610d51cc9d92626d5c7da401769f4c83ac
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-07 03:05:42 -0700

    Added initial files for task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Module for function that prints a square with # char
+"""
+
+
+def print_square(size):
+    """
+    Args:
+        size: The size of the square
+    Raises:
+        TypeError: If size is not an int
+        ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
