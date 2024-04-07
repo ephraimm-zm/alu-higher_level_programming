7a4e6841208ee12adbbe654ec0be6f5d24da1d15
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-07 02:48:24 -0700

    Added intial files for task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+Module for function that prints a message
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Args:
+        first_name and last_name
+    Raises:
+        TypeError: if first_name or last_name is not a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
