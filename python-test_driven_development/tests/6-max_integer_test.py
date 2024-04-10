436c94cfbabca235cfc5eb97f11036e2bfccd05a
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-10 00:44:32 -0700

    Fixed typo on import module

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,7 +2,7 @@
 """
 Unit tests for task 6 using unittest module
 """
-import uninttest
+import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
