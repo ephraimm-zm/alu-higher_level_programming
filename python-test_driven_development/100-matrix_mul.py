32df3eebd1e85686ca84cd00e2f79dce1089821a
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-10 01:14:28 -0700

    Added initial solution for task 100

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+Module for function that multiplies two matrices
+"""
+
+
+def matrix_mul(m_a, m_b):
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+
+    for row in m_a:
+        for elem in row:
+            if not isinstance(elem, (int, float)):
+                raise TypeError(
+                        "m_a should contain only integers or floats"
+                        )
+    if len(set(len(row) for row in m_a)) != 1:
+        raise ValueError("each row of m_a must be of the same size")
+
+    if len(set(len(row) for row in m_b)) != 1:
+        raise ValueError("each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    return result_matrix
