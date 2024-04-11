17efbe45edf45789742649a1863f20aa281de1ad
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-11 03:00:23 -0700

    Added docustrings

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,9 +5,24 @@ Module for class that will Base of other classes
 
 
 class Base:
+    """
+    Base class for other classes.
+    
+    Atribues:
+        __nb_objects (int): Number of objects created.
+        id (int): Identifier for the object
+    """
     __nb_object = 0
 
     def __init__(self, id=None):
+        """
+        Initializes a base object
+
+        Args:
+            id (int): Identifier for object
+        Returns:
+            None
+        """
         if id is not None:
             self.id = id
         else:
