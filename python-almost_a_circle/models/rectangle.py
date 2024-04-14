f63d686420f581bc49c1cc0dc7c50cabfded0026
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-14 07:41:43 -0700

    Updated update method to handle cases when len of args is less than 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -131,11 +131,16 @@ class Rectangle(Base):
         *args: Variable number of arguments.
         """
         if args:
-            self.id = args[0]
-            self.width = args[1]
-            self.height = args[2]
-            self.x = args[3]
-            self.y = args[4]
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
         else:
             for key, value in kwargs.items():
                 if key == 'id':
