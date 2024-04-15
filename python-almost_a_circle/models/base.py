d7e8085c9e3aa7042a5bbe55d0f16684ade64fba
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-15 06:15:37 -0700

    Added to json string method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -60,3 +60,18 @@ class Base:
                         for obj in list_objs
                 ]
                 file.write(cls.to_json_string(dictionaries))
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Serialize a list of dictionaries into a JSON string
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        Returns:
+            str: A JSON string representation of list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+                
