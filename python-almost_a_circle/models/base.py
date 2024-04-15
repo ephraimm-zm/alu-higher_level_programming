d6b10e2aa1d5a134aa601b7493d1ee05cfe48645
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-15 03:28:22 -0700

    Updated base class to include method to return string json string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Module for class that will Base of other classes
 """
+import json
 
 
 class Base:
@@ -27,3 +28,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Serialize a list of dictionaries into a JSON string
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        Returns:
+            str: A JSON string representaion of list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
