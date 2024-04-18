b405e9be507578d7037b0fa84fd477deb65990c3
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-18 02:15:52 -0700

    Added initial solution for task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+List all states from the specified database.
+"""
+import MySQLdb
+import sys
+
+
+def list_states(username, password, database_name):
+    """Connect to the database and list all states sorted by ID."""
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name
+            )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
+
+    if __name__ == "__main__":
+        """Main function."""
+        username= sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        list_states(username, password, database_name)
