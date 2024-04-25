96b437945b08f0d1902249d4a5f4b87d6b8ce629
ephraimm-zm <e.mulilo1@alustudent.com>
2024-04-25 05:25:21 -0700

    Added initial solution for task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+
+
+import sys
+import MySQLdb
+
+def main():
+    """
+    Main function to get state information from the database
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    connection = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db = database
+            )
+    cursor = connection.cursor()
+    query = """
+    SELECT * 
+    FROM states
+    WHERE name = %s
+    ORDER BY id ASC
+    """
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
+
+if __name__ == "__main__":
+    main()
