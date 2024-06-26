1849372680b83760f176b8f7443c2a99e07bcf29
ephraimm-zm <e.mulilo1@alustudent.com>
2024-06-26 01:32:43 -0700

    semi standard fix

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let line of lines) {
-	console.log(line);
+for (const line of lines) {
+  console.log(line);
 }
