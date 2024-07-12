8ff86865574c574729ac0f7f880418a50f8d8a12
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-12 05:34:22 -0700

    semi standard fix for file 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const argv = process.argv.slice(2);
 
 if (argv.length === 0) {
-	console.log("No argument");
+  console.log('No argument');
 } else if (argv.length === 1) {
-	console.log("Argument found");
+  console.log('Argument found');
 } else if (argv.length > 1) {
-	console.log("Arguments found");
+  console.log('Arguments found');
 }
