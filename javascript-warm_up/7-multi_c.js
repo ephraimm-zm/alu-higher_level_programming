89b05bbccaa203bb0701c03cc4c767f7e599a6a1
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-12 06:31:33 -0700

    Added initial solution for task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const value = parseInt(process.argv[2]);
+
+if (!isNaN(value) && value > 0) {
+  for (let x = 0; x < value; x++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
