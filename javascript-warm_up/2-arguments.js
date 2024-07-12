d46b37b337dc89b60f618bff26d1ffff63870e86
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-12 05:31:35 -0700

    Added initial solution for task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const argv = process.argv.slice(2);
+
+if (argv.length === 0) {
+	console.log("No argument");
+} else if (argv.length === 1) {
+	console.log("Argument found");
+} else if (argv.length > 1) {
+	console.log("Arguments found");
+}
