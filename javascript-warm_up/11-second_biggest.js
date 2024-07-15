62df644d43dd22aa4f9cb4e039fb5dad71665cff
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-15 02:27:43 -0700

    Added initial solution for task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+const argv = process.argv.slice(2);
+
+if (argv.length === 0) {
+	console.log("0");
+	process.exit();
+}
+
+if (argv.length === 1) {
+	console.log("1");
+	process.exit();
+}
+
+let highest = Number(argv[0]);
+let second = Number.NEGATIVE_INFINITY;
+
+for (let i = 1; i < argv.length; i++) {
+	let num = Number(argv[i]);
+
+	if (num > highest) {
+		second = highest
+		highest = num;
+	} else if (num > second && num < highest) {
+		second = num;
+	}
+}
+
+console.log(second);
