ee0ff58c8a0c55c91f14fd549c486854ed76ff4c
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-16 00:18:33 -0700

    Semi standard fixed 9 and 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,27 +2,27 @@
 const argv = process.argv.slice(2);
 
 if (argv.length === 0) {
-	console.log("0");
-	process.exit();
+  console.log('0');
+  process.exit();
 }
 
 if (argv.length === 1) {
-	console.log("1");
-	process.exit();
+  console.log('1');
+  process.exit();
 }
 
 let highest = Number(argv[0]);
 let second = Number.NEGATIVE_INFINITY;
 
 for (let i = 1; i < argv.length; i++) {
-	let num = Number(argv[i]);
+  const num = Number(argv[i]);
 
-	if (num > highest) {
-		second = highest
-		highest = num;
-	} else if (num > second && num < highest) {
-		second = num;
-	}
+  if (num > highest) {
+    second = highest;
+    highest = num;
+  } else if (num > second && num < highest) {
+    second = num;
+  }
 }
 
 console.log(second);
