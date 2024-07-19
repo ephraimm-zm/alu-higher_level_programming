fa82830e85200fb208b3d75b1a601d172c470019
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-19 01:39:53 -0700

    Semi standard fix

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -12,15 +12,17 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
-	rotate() {
-		let temp = this.width;
-		this.width = this.height;
-		this.height = temp;
-	}
-	double() {
-		this.width *= 2;
-		this.height *= 2;
-	}
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
