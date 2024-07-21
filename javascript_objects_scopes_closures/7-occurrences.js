67f66fc74b6dfc8eabc7e3367912f22193e1505e
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-21 00:43:15 -0700

    Corrected function name

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.nb0curences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
   for (const element of list) {
     if (element === searchElement) {
