660f43b88fd2695119bf26cd25ed49af255e661d
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-21 00:40:39 -0700

    Added initial solution for task 7

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+exports.nb0curences = function (list, searchElement) {
+  let counter = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      counter++;
+    }
+  }
+  return counter;
+};
