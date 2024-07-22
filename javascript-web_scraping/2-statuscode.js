b41b3e22d7e5c36f12c6e2845620582c0f102702
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-22 09:58:07 +0000

    Added initial solution for task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const request = require('request');
+const urlPath = process.argv[2];
+
+request.get(urlPath, (error, response, body) => {
+if (error) {
+console.error(error);
+} else {
+console.log("code: " = response.statusCode);
+}
+});
