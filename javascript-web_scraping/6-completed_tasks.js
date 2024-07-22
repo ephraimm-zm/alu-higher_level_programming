9fb54ffe93ffb3972584eaccebb3121193b13688
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-22 19:57:25 +0000

    Added intitial solution for task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+const requests = require('requests');
+
+const targetURL = process.argv[2];
+
+requests.get(targetURL, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const users = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (users[todo.userId]) {
+        users[todo.userId]++;
+      } else {
+        users[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(users);
+});
