9475c00adcd791ab28b23546390b0765625a2e8a
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-22 10:36:36 +0000

    Semi standard fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 const urlPath = process.argv[2];
 
 request.get(urlPath, (error, response, body) => {
-if (error) {
-console.error(error);
-} else {
-console.log("code: " + response.statusCode);
-}
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
