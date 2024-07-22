5f804bfa75e83efdeba85e3c87f998cf477183bc
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-22 14:24:35 +0000

    Added initial solution to task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const path = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(path, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
