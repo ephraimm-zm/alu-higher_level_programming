6db01bdb980910599377287b3d7d17726a30552b
ephraimm-zm <e.mulilo1@alustudent.com>
2024-07-22 12:38:01 +0000

    Added initial solution to task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    let count = 0;
+
+    data.characters.forEach(character => {
+      if (character.endsWith('18/')) {
+        count++;
+      }
+    });
+    console.log(count);
+  }
+});
